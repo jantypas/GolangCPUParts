455548caa2d0d89c8784bc4028b78c034dfa4909
John Antypas <ja@antypas.net>
2025-01-21 20:17:29 -0800

    Updated

--- MMUSupport/ProcessMemory/ProcessSupport.go
+++ MMUSupport/ProcessMemory/ProcessSupport.go
@@ -1,6 +1,9 @@
 package ProcessMemory
 
 import (
+	"GolangCPUParts/MMUSupport"
+	"GolangCPUParts/MMUSupport/PhysicalMemory"
+	"GolangCPUParts/MMUSupport/VirtualMemory"
 	"time"
 )
 
@@ -13,8 +16,33 @@ type ProcessObject struct {
 	GID       uint32
 	CreatedOn time.Time
 	System    bool
+	Memory    []uint32
 }
 
 type ProcessTable struct {
-	Processes map[uint16]ProcessObject
+	Processes      map[uint16]ProcessObject
+	NextPID        uint16
+	Swapper        MMUSupport.SwapperInterface
+	PhysicalMemory PhysicalMemory.PhysicalMemoryContainer
+	VirtualMemory  VirtualMemory.VMContainer
+}
+
+func ProcessTable_Initialize(name string, sz uint32) (*ProcessTable, error) {
+	pt := ProcessTable{}
+	pt.Processes = make(map[uint16]ProcessObject)
+	pt.NextPID = 0
+	pt.Swapper.Filename = "/tmp/swap.swp"
+	pmem, err := PhysicalMemory.PhysicalMemory_Initialize(name)
+	if err != nil {
+		return nil, err
+	}
+	pt.PhysicalMemory = *pmem
+	vmem, err := VirtualMemory.VirtualMemory_Initiailize(*pmem, pt.Swapper, sz)
+	pt.VirtualMemory = *vmem
+	return &pt, nil
+}
+
+func (pt *ProcessTable) Terminate() {
+	pt.PhysicalMemory.Terminate()
+	pt.VirtualMemory.Terminate()
 }
